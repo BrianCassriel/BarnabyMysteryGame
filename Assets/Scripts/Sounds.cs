062081d42d6a89da53e814649e45c0e67251e486
elowy4 <elowy@chapman.edu>
2023-05-09 18:29:50 -0700

    starting to wire up sounds but they don't actually do anything yet

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioClip GameStartSound;
+    public AudioClip GameLoseSound;
+    public AudioClip OpenDoor;
+    public AudioClip FinalGuess;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
